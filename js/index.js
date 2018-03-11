35c66ce5c8e5a1e4e0d1b75d9c49003b1fba492b
madhavkolla <kollavmadhav@gmail.com>
2018-03-11 00:06:10 -0600

    added js

--- js/index.js
+++ js/index.js
@@ -46,3 +46,19 @@ $('.port-item').click(function () {
       if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
     }
   }
+
+  function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+      while (arr1.indexOf(arr2[i]) > -1) {
+        arr1.splice(arr1.indexOf(arr2[i]), 1);
+      }
+    }
+    element.className = arr1.join(" ");
+  }
+
+  $('.flip-container').click(function () {
+    $('.flip-container').toggleClass('flip');
+  });
